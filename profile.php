a8118012471a4b57f3d47e28d6a6f279d0220a96
Kall <samueluche432@gmail.com>
2023-10-30 23:10:50 +0100

    More changes to profile.php

--- profile.php
+++ profile.php
@@ -1,19 +1,42 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title></title>
+  <title>Qface | Profile</title>
  </head>
  <style type="text/css"> 
 #blue_bar{
-height: 50px;
+height: 60px;
 background-color: #405d9b;
 color:#d9dfeb;
 }
+#search_box {
+      width: 400px;
+      height: 4px;
+      border-radius: 4px;
+      border: solid 1px #aaa;
+      padding: 9px;
+      font-size: 10px;
+      margin-top: 6px;
+      background-image: url(search.png);
+      background-repeat: no-repeat;
+      background-position: right;
+    }
 </style>
- <body>
+ <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;  background-color: #d0d8e4;">
+
+ <!-- Top bar-->
 <div id="blue_bar">
-
+ <div style="width:800px; margin:auto; font: size 40px;">
+  Qface &nbsp;
+  <input type="text" id="search_box" placeholder="Search for people">
+  <img src="selfie.jpg" style="width: 60px; float:right;">
+ </div>
 </div>
-
- </body>
+ <!-- Cover area-->
+ <div  style="width:800px; margin:auto; background-color: black; min-height:  400px;">
+<div style="background-color: white; text-align: center; color:#405d9b;">
+<img src="mountain.jpg" style="width: 100%;">
+</div>
+ </div>
+</body>
 </html>
