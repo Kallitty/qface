6e18681048984b0c79f229589be4256057337b6c
Kall <samueluche432@gmail.com>
2023-11-04 17:39:28 +0100

    A connection to the data base

--- connect.php
+++ connect.php
@@ -1,6 +1,14 @@
 <?php
-$number=30;
-if($number>10){
-echo"Too high";
-}
+
+$host="localhost";
+$username="root";
+$password="";
+$db="qface_db";
+
+$connection=mysqli_connect($host, $username,$password, $db);
+$first_name="Kangaroo";
+$last_name="Peters";
+$query= "insert into users(first_name,last_name) values('$first_name', '$last_name')";
+mysqli_query($connection, $query);
+
 ?> 
