a3d2239824e6d2605ef792cd649a09551cd64d97
Kall <samueluche432@gmail.com>
2023-11-05 06:16:58 +0100

    Modified connect.php

--- connect.php
+++ connect.php
@@ -1,21 +1,55 @@
 <?php
 
-$host="localhost";
-$username="root";
-$password="";
-$db="qface_db";
+class Database {
 
-$connection=mysqli_connect($host, $username,$password, $db);
+   private $host="localhost";
+   private $username="root";
+   private $password="";
+   private $db="qface_db";
 
-$query= "select * from users";
-$result=mysqli_query($connection, $query);
-
-while($row=mysqli_fetch_assoc($result))
+ function connect(){
+$connection=mysqli_connect($this->host, $this->username, $this->password, $this->db);
+return $connection;
+ }
+ function read($query){
+  $conn= $this->connect(); 
+  $result=mysqli_query($conn, $query);
+  if (!$result){
+     return false;
+    }else{
+     $data=false;
+     while($row=mysqli_fetch_assoc($result))
 {
-echo "<pre>";
-print_r($row);
-echo "</pre>";
+$data[]= $row;
+return $data;
 }
+    }
+ }
+ function save($query){
+   $conn= $this->connect();
+    $result= mysqli_query($conn, $query);
+    if (!$result){
+     return false;
+    }else{
+     return true;
+    }
+ }
+}
+$DB= new Database();
+$query="select * from users";
+$data= $DB->read($query);
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+
+
+
+
+
+
+
 
 
 
