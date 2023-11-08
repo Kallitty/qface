61aa9c4c367bc2ed9d4a958810f8e7dd6d1eb489
Kall <samueluche432@gmail.com>
2023-11-08 02:41:50 +0100

    adding to the email checks

--- classes/signup.php
+++ classes/signup.php
@@ -1,67 +1,65 @@
 <?php
 
 class Signup{
- private $error="";
- public function evaluate($data){
- foreach ($data as $key=>$value){
-  #checks
-  if (empty($value)){
-   $this->error= $this->error . $key . "Kindly input field</br>";
-  }
-  if ($key=="email"){
-   // if(!preg_match("'/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})*$/';", $value)){
-     if(!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $value)){
-    $this->error= $this->error . "invalid email address!</br>";
-   }
-  }
+    private $error = "";
 
- }if ($key=="first_name"){
-   if(is_numeric($value)){
-    $this->error= $this->error . "First name can not be a number!</br>";
-   }
-  }
+    public function evaluate($data){
+        foreach ($data as $key => $value) {
+            // Check for empty fields
+            if (empty($value)) {
+                $this->error = $this->error . $key . " Kindly input field</br>";
+            }
 
-  if ($key=="surname"){
-   if(is_numeric($value)){
-    $this->error= $this->error . "Surname can not be a number!</br>";
-   }
-  }
-//check for password
-// if($value['password'] == $value['confirmpassword']) {
-// invalid email...
+            if ($key == "email_address_or_phone_number") {
+                if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->error = $this->error . "Invalid email address!</br>";
+                }
+            }
+            
 
- if ($this->error ==""){
-// no error
-$this->create_user($data);
- }else
- {
-  return $this->error;
- }
- }
- public function create_user($data){
-  $first_name=$data['first_name'];
-  $surname=$data['surname'];
-  $middle_name=$data['middle_name'];
-  $gender=$data['gender'];
-  $email_address_or_phone_number=$data['email_address_or_phone_number'];
-  $password=$data['password'];
-  //created these, take note
-  $url_address=strtolower($first_name) ."." . strtolower($surname);
-  $userid=$this->create_userid();
+            if ($key == "first_name" && is_numeric($value)) {
+                $this->error = $this->error . "First name can not be a number!</br>";
+            }
 
-  $query="insert into users(userid, first_name, surname, gender, email_address_or_phone_number, password, url_address, middle_name) values('$userid', '$first_name', '$surname', '$gender', '$email_address_or_phone_number', '$password', '$url_address', '$middle_name')";
-  // echo $query;
-  $DB=new Database();
-  $DB->save($query);
- }
- //userid loop
- private  function create_userid(){
-  $length=rand(4, 19);
-  $number ="";
-  for ($i=0; $i< $length; $i++){
-$new_rand= rand(0,9);
-$number = $number . $new_rand;
-  }
-  return $number;
- }
+            if ($key == "surname" && is_numeric($value)) {
+                $this->error = $this->error . "Surname can not be a number!</br>";
+            }
+        }
+
+        if ($this->error === "") {
+            // No error
+            $this->create_user($data);
+        } else {
+            return $this->error;
+        }
+    }
+
+    public function create_user($data){
+        $first_name = $data['first_name'];
+        $surname = $data['surname'];
+        $middle_name = $data['middle_name'];
+        $gender = $data['gender'];
+        $email_address_or_phone_number = $data['email_address_or_phone_number'];
+        $password = $data['password'];
+
+        // Create other variables as needed
+
+        $url_address = strtolower($first_name) . "." . strtolower($surname);
+        $userid = $this->create_userid();
+
+        $query = "insert into users(userid, first_name, surname, gender, email_address_or_phone_number, password, url_address, middle_name) values('$userid', '$first_name', '$surname', '$gender', '$email_address_or_phone_number', '$password', '$url_address', '$middle_name')";
+
+        // Execute your database query here
+    }
+
+    private function create_userid(){
+        $length = rand(4, 19);
+        $number = "";
+        for ($i = 0; $i < $length; $i++) {
+            $new_rand = rand(0, 9);
+            $number = $number . $new_rand;
+        }
+        return $number;
+    }
 }
+?>
