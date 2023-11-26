3b4c09e340cebdf6f24af2d198bbb8d3abebe48f
Kall <samueluche432@gmail.com>
2023-11-26 23:53:44 +0100

    modified:   profile.php

--- profile.php
+++ profile.php
@@ -128,7 +128,7 @@ color:#d9dfeb;
  <!-- Top bar-->
 <div id="blue_bar">
  <div style="width:800px; margin:auto; font: size 40px;">
-  Qface &nbsp;
+  <a href="index.php" style="text-decoration: none; color:#d0d8e4;"> Qface &nbsp; </a> 
   <input type="text" id="search_box" placeholder="Search for people">
   <img src="selfie.jpg" style="width: 60px; float:right;">  <br/>
   <a href="logout.php">
