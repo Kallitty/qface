add95f68f4b906a4f00eb2a5dcea799df3cb1091
Kall <samueluche432@gmail.com>
2023-11-27 14:40:19 +0100

    new file:   header.php modified:   profile.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- Top bar-->
+<div id="blue_bar">
+ <div style="width:800px; margin:auto; font: size 40px;">
+  <a href="index.php" style="text-decoration: none; color:#d0d8e4;"> Qface &nbsp; </a> 
+  <input type="text" id="search_box" placeholder="Search for people">
+  <img src="selfie.jpg" style="width: 60px; float:right;">  <br/>
+  <a href="logout.php">
+    <span style="font: size 11px; float:right; margin: 5px; color:azure; ">Logout</span>
+  </a>
+  
+ </div>
+</div>
--- profile.php
+++ profile.php
@@ -125,18 +125,9 @@ color:#d9dfeb;
 </style>
  <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;  background-color: #d0d8e4;">
 
- <!-- Top bar-->
-<div id="blue_bar">
- <div style="width:800px; margin:auto; font: size 40px;">
-  <a href="index.php" style="text-decoration: none; color:#d0d8e4;"> Qface &nbsp; </a> 
-  <input type="text" id="search_box" placeholder="Search for people">
-  <img src="selfie.jpg" style="width: 60px; float:right;">  <br/>
-  <a href="logout.php">
-    <span style="font: size 11px; float:right; margin: 5px; color:azure; ">Logout</span>
-  </a>
-  
- </div>
-</div>
+ <?php 
+//  include("header.php");
+ ?>
  <!-- Cover area-->
  <div  style="width:800px; margin:auto;  min-height:  400px;">
 <div style="background-color: white; text-align: center; color:#405d9b;">
@@ -147,7 +138,7 @@ color:#d9dfeb;
 echo $user_data['first_name'] .' '. $user_data['surname'];
 ?>
 <br/>
-<a href="index.php" >  <div id="menu_button">Timeline</div>  </a>
+<a href="index.php" style="text-decoration: none;">  <div id="menu_button" >Timeline</div>  </a>
 <div id="menu_button">About</div>  
 <div  id="menu_button">Friends </div>
 <div  id="menu_button">Photos </div>
