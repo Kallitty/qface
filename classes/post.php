050161dd7f0088aad5ce59a609ce83453fd8c9d8
Kall <samueluche432@gmail.com>
2024-01-15 23:33:13 +0100

    modified:   classes/post.php

--- classes/post.php
+++ classes/post.php
@@ -3,10 +3,29 @@ class Post{
  private $error="";
 
 public function create_post($userid, $data, $files){
-if (!empty($data['post']) || !empty($files['file'] ['name'] )){
+if (!empty($data['post']) || !empty($files['file'] ['name'] ))
+{
+    $myimage= "";
+    $has_image= 0;
+    if (!empty($files['file'] ['name'] ))
+    {
+        $folder="uploads/" . $user_id ."/";
+        //create folder
+        if(!file_exists($folder))
+        {
+          mkdir($folder, 0777, true);
+        }
+       $image_class = new Image;
+        $myimage= $folder . $image_class->generate_filename(15) . ".jpg";
+
+    move_uploaded_file($_FILES['file'] ['tmp_name'], $myimage);
+    $image_class->resize_image($myimage, $filename, 1500, 1500);
+
+        $has_image= 1;
+    }
 $post =addslashes($data['post']);
 $postid=$this->create_postid();
-$query="insert into posts (userid, postid, post) values ('$userid', '$postid', '$post')";
+$query="insert into posts (userid, postid, post, image, has_image) values ('$userid', '$postid', '$post', '$myimage', '$has_image')";
 $DB = new Database();
 $DB->save($query);
 }else{
